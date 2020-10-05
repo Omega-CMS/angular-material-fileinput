4074f33d3c37fdfcfc2ac3ce0add8a3e44a11cd9
developer@omegacms.io <developer@omegacms.io>
2020-10-05 14:19:33 -0400

    Updating dependencies, adding ngFileEvent functionality to directive

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Based on https://github.com/shuyu/angular-material-fileinput
 ```
 
 ## Release History
+* v1.6.0
+    * Updating dependencies, adding ngFileEvent functionality to directive
 * v1.0.1
     * Update to YARN.
 * v1.0.0
--- dist/omega-cms-lf-ng-md-file-input.js
+++ dist/omega-cms-lf-ng-md-file-input.js
@@ -64,9 +64,9 @@
         return lfFileObj;
     }
 
-    var lfNgMdFileinput = angular.module('lfNgMdFileInput', ['ngMaterial']);
+    var omegaCmsLfNgMdFileInput = angular.module('omegaCmsLfNgMdFileInput', ['ngMaterial']);
 
-    lfNgMdFileinput.directive('lfFile', function() {
+    omegaCmsLfNgMdFileInput.directive('lfFile', function() {
         return {
             restrict: 'E',
             scope: {
@@ -107,8 +107,8 @@
                         }
                         element.replaceWith(
                             '<object type="' + fileType + '" data="' + src + '">' +
-                                '<div class="lf-ng-md-file-input-preview-default">' +
-                                    '<md-icon class="lf-ng-md-file-input-preview-icon '+unKnowClass+'"></md-icon>' +
+                                '<div class="omega-cms-lf-ng-md-file-input-preview-default">' +
+                                    '<md-icon class="omega-cms-lf-ng-md-file-input-preview-icon '+unKnowClass+'"></md-icon>' +
                                 '</div>' +
                             '</object>'
                         );
@@ -118,35 +118,35 @@
         };
     });
 
-    lfNgMdFileinput.run(['$templateCache', function($templateCache){
-        $templateCache.put('lfNgMdFileinput.html', [
-            '<div layout="column" class="lf-ng-md-file-input" ng-model="'+genLfObjId()+'">',
-                '<div layout="column" class="lf-ng-md-file-input-preview-container" ng-class="{\'disabled\':isDisabled}" ng-show="isDrag || (isPreview && lfFiles.length)">',
-                    '<md-button aria-label="remove all files" class="close lf-ng-md-file-input-x" ng-click="removeAllFiles($event)" ng-hide="!lfFiles.length || !isPreview" >&times;</md-button>',
-                    '<div class="lf-ng-md-file-input-drag">',
-                        '<div layout="row" layout-align="center center" class="lf-ng-md-file-input-drag-text-container" ng-show="(!lfFiles.length || !isPreview) && isDrag">',
-                            '<div class="lf-ng-md-file-input-drag-text">{{strCaptionDragAndDrop}}</div>',
+    omegaCmsLfNgMdFileInput.run(['$templateCache', function($templateCache){
+        $templateCache.put('omegaCmsLfNgMdFileInput.html', [
+            '<div layout="column" class="omega-cms-lf-ng-md-file-input" ng-model="'+genLfObjId()+'">',
+                '<div layout="column" class="omega-cms-lf-ng-md-file-input-preview-container" ng-class="{\'disabled\':isDisabled}" ng-show="isDrag || (isPreview && lfFiles.length)">',
+                    '<md-button aria-label="remove all files" class="close omega-cms-lf-ng-md-file-input-x" ng-click="removeAllFiles($event)" ng-hide="!lfFiles.length || !isPreview" >&times;</md-button>',
+                    '<div class="omega-cms-lf-ng-md-file-input-drag">',
+                        '<div layout="row" layout-align="center center" class="omega-cms-lf-ng-md-file-input-drag-text-container" ng-show="(!lfFiles.length || !isPreview) && isDrag">',
+                            '<div class="omega-cms-lf-ng-md-file-input-drag-text">{{strCaptionDragAndDrop}}</div>',
                         '</div>',
-                        '<div class="lf-ng-md-file-input-thumbnails" ng-if="isPreview == true">',
-                            '<div class="lf-ng-md-file-input-frame" ng-repeat="lffile in lfFiles" ng-click="onFileClick(lffile)">',
-                                '<div class="lf-ng-md-file-input-x" aria-label="remove {{lffile.lFfileName}}" ng-click="removeFile(lffile,$event)">&times;</div>',
+                        '<div class="omega-cms-lf-ng-md-file-input-thumbnails" ng-if="isPreview == true">',
+                            '<div class="omega-cms-lf-ng-md-file-input-frame" ng-repeat="lffile in lfFiles" ng-click="onFileClick(lffile)">',
+                                '<div class="omega-cms-lf-ng-md-file-input-x" aria-label="remove {{lffile.lFfileName}}" ng-click="removeFile(lffile,$event)">&times;</div>',
                                 '<lf-file lf-file-obj="lffile" lf-unknow-class="strUnknowIconCls"/>',
                                 // '<md-progress-linear md-mode="indeterminate"></md-progress-linear>',
-                                '<div class="lf-ng-md-file-input-frame-footer">',
-                                    '<div class="lf-ng-md-file-input-frame-caption">{{lffile.lfFileName}}</div>',
+                                '<div class="omega-cms-lf-ng-md-file-input-frame-footer">',
+                                    '<div class="omega-cms-lf-ng-md-file-input-frame-caption">{{lffile.lfFileName}}</div>',
                                 '</div>',
                             '</div>',
                         '</div>',
                         '<div class="clearfix" style="clear:both"></div>',
                     '</div>',
                 '</div>',
-                '<div layout="row" class="lf-ng-md-file-input-container" >',
-                    '<div class="lf-ng-md-file-input-caption" layout="row" layout-align="start center" flex ng-class="{\'disabled\':isDisabled}" >',
+                '<div layout="row" class="omega-cms-lf-ng-md-file-input-container" >',
+                    '<div class="omega-cms-lf-ng-md-file-input-caption" layout="row" layout-align="start center" flex ng-class="{\'disabled\':isDisabled}" >',
                         '<md-icon class="lf-icon" ng-class="strCaptionIconCls"></md-icon>',
-                        '<div flex class="lf-ng-md-file-input-caption-text-default" ng-show="!lfFiles.length">',
+                        '<div flex class="omega-cms-lf-ng-md-file-input-caption-text-default" ng-show="!lfFiles.length">',
                             '{{strCaptionPlaceholder}}',
                         '</div>',
-                        '<div flex class="lf-ng-md-file-input-caption-text" ng-hide="!lfFiles.length">',
+                        '<div flex class="omega-cms-lf-ng-md-file-input-caption-text" ng-hide="!lfFiles.length">',
                             '<span ng-if="isCustomCaption">{{strCaption}}</span>',
                             '<span ng-if="!isCustomCaption">',
                                 '{{ lfFiles.length == 1 ? lfFiles[0].lfFileName : lfFiles.length+" files selected" }}',
@@ -154,31 +154,31 @@
                         '</div>',
                         '<md-progress-linear md-mode="determinate" value="{{floatProgress}}" ng-show="intLoading && isProgress"></md-progress-linear>',
                     '</div>',
-                    '<md-button aria-label="remove all files" ng-disabled="isDisabled" ng-click="removeAllFiles()" ng-hide="!lfFiles.length || intLoading" class="md-raised lf-ng-md-file-input-button lf-ng-md-file-input-button-remove" ng-class="strRemoveButtonCls">',
+                    '<md-button aria-label="remove all files" ng-disabled="isDisabled" ng-click="removeAllFiles()" ng-hide="!lfFiles.length || intLoading" class="md-raised omega-cms-lf-ng-md-file-input-button omega-cms-lf-ng-md-file-input-button-remove" ng-class="strRemoveButtonCls">',
                         '<md-icon class="lf-icon" ng-class="strRemoveIconCls"></md-icon> ',
                         '{{strCaptionRemove}}',
                     '</md-button>',
-					'<md-button aria-label="submit" ng-disabled="isDisabled" ng-click="onSubmitClick()" class="md-raised md-warn lf-ng-md-file-input-button lf-ng-md-file-input-button-submit" ng-class="strSubmitButtonCls" ng-show="lfFiles.length && !intLoading && isSubmit">',
+					'<md-button aria-label="submit" ng-disabled="isDisabled" ng-click="onSubmitClick()" class="md-raised md-warn omega-cms-lf-ng-md-file-input-button omega-cms-lf-ng-md-file-input-button-submit" ng-class="strSubmitButtonCls" ng-show="lfFiles.length && !intLoading && isSubmit">',
                         '<md-icon class="lf-icon" ng-class="strSubmitIconCls"></md-icon> ',
                         '{{strCaptionSubmit}}',
                     '</md-button>',
-                    '<md-button aria-label="browse" ng-disabled="isDisabled" ng-click="openDialog($event, this)" class="md-raised lf-ng-md-file-input-button lf-ng-md-file-input-button-brower" ng-class="strBrowseButtonCls">',
+                    '<md-button aria-label="browse" ng-disabled="isDisabled" ng-click="openDialog($event, this)" class="md-raised omega-cms-lf-ng-md-file-input-button omega-cms-lf-ng-md-file-input-button-brower" ng-class="strBrowseButtonCls">',
                         '<md-icon class="lf-icon" ng-class="strBrowseIconCls"></md-icon> ',
                         '{{strCaptionBrowse}}',
-                        '<input type="file" aria-label="{{strAriaLabel}}" accept="{{accept}}" ng-disabled="isDisabled" class="lf-ng-md-file-input-tag" />',
+                        '<input type="file" aria-label="{{strAriaLabel}}" accept="{{accept}}" ng-disabled="isDisabled" class="omega-cms-lf-ng-md-file-input-tag" />',
                     '</md-button>',
                 '</div>',
             '</div>'
         ].join(''));
     }]);
 
-    lfNgMdFileinput.filter('lfTrusted', ['$sce', function ($sce) {
+    omegaCmsLfNgMdFileInput.filter('lfTrusted', ['$sce', function ($sce) {
         return function(url) {
             return $sce.trustAsResourceUrl(url);
         };
     }]);
 
-    lfNgMdFileinput.directive('lfRequired', function() {
+    omegaCmsLfNgMdFileInput.directive('lfRequired', function() {
         return {
             restrict: "A",
             require:"ngModel",
@@ -196,7 +196,7 @@
         }
     });
 
-    lfNgMdFileinput.directive('lfMaxcount', function() {
+    omegaCmsLfNgMdFileInput.directive('lfMaxcount', function() {
         return {
             restrict: "A",
             require:"ngModel",
@@ -220,7 +220,7 @@
         }
     });
 
-    lfNgMdFileinput.directive('lfFilesize', function() {
+    omegaCmsLfNgMdFileInput.directive('lfFilesize', function() {
         return {
             restrict: "A",
             require:"ngModel",
@@ -267,7 +267,7 @@
         }
     });
 
-	lfNgMdFileinput.directive('lfTotalsize', function() {
+	omegaCmsLfNgMdFileInput.directive('lfTotalsize', function() {
         return {
             restrict: "A",
             require:"ngModel",
@@ -309,7 +309,7 @@
         }
     });
 
-    lfNgMdFileinput.directive('lfMimetype', function() {
+    omegaCmsLfNgMdFileInput.directive('lfMimetype', function() {
         return {
             restrict: "A",
             require:"ngModel",
@@ -342,10 +342,10 @@
         }
     });
 
-    lfNgMdFileinput.directive('lfNgMdFileInput',['$q','$compile','$timeout', function($q,$compile,$timeout){
+    omegaCmsLfNgMdFileInput.directive('omegaCmsLfNgMdFileInput',['$q','$compile','$timeout', function($q,$compile,$timeout){
         return {
             restrict: 'E',
-            templateUrl: 'lfNgMdFileinput.html',
+            templateUrl: 'omegaCmsLfNgMdFileInput.html',
             replace: true,
             require:"ngModel",
             scope:{
@@ -363,13 +363,14 @@
                 lfOnFileRemove: '=?',
                 accept:'@?',
                 ngDisabled:'=?',
-				ngChange: '&?'
+				ngChange: '&?',
+				ngFileEvent: '&?'
             },
             link: function(scope,element,attrs,ctrl){
 
-                var elFileinput = angular.element(element[0].querySelector('.lf-ng-md-file-input-tag'));
-                var elDragview  = angular.element(element[0].querySelector('.lf-ng-md-file-input-drag'));
-                var elThumbnails = angular.element(element[0].querySelector('.lf-ng-md-file-input-thumbnails'));
+                var elFileinput = angular.element(element[0].querySelector('.omega-cms-lf-ng-md-file-input-tag'));
+                var elDragview  = angular.element(element[0].querySelector('.omega-cms-lf-ng-md-file-input-drag'));
+                var elThumbnails = angular.element(element[0].querySelector('.omega-cms-lf-ng-md-file-input-thumbnails'));
                 var intFilesCount = 0;
 
                 scope.intLoading = 0;
@@ -545,6 +546,7 @@
 				scope.removeAllFiles = function(event){
 					scope.removeAllFilesWithoutVaildate();
                     executeValidate();
+                    onFileEvent('removeAllFiles');
 				};
 
 				scope.removeFileByName = function(name,event){
@@ -559,6 +561,7 @@
 						return true;
 					});
                     executeValidate();
+                    onFileEvent('removeFileByName');
 				};
 
                 scope.removeFile = function(lfFile) {
@@ -573,6 +576,7 @@
 						return true;
 					});
                     executeValidate();
+                    onFileEvent('removeFile');
                 };
 
 				//call back function
@@ -601,7 +605,7 @@
                     if(scope.isDisabled || !scope.isDrag){
                         return;
                     }
-                    elDragview.addClass("lf-ng-md-file-input-drag-hover");
+                    elDragview.addClass("omega-cms-lf-ng-md-file-input-drag-hover");
                 });
 
 				elDragview.bind("dragleave", function(e){
@@ -610,7 +614,7 @@
 					if(scope.isDisabled || !scope.isDrag){
 						return;
 					}
-					elDragview.removeClass("lf-ng-md-file-input-drag-hover");
+					elDragview.removeClass("omega-cms-lf-ng-md-file-input-drag-hover");
 				});
 
 				elDragview.bind("drop", function(e){
@@ -619,7 +623,7 @@
 					if(scope.isDisabled || !scope.isDrag){
 						return;
 					}
-					elDragview.removeClass("lf-ng-md-file-input-drag-hover");
+					elDragview.removeClass("omega-cms-lf-ng-md-file-input-drag-hover");
                     if(angular.isObject(e.originalEvent)){
                         e = e.originalEvent;
                     }
@@ -676,6 +680,12 @@
                     ctrl.$validate();
                 }
 
+                var onFileEvent = function(eventName) {
+					if(angular.isFunction(scope.ngFileEvent)){
+						scope.ngFileEvent({ event: eventName, files: scope.lfFiles });
+					}
+                }
+
 				var readFile = function(file){
 					readAsDataURL(file).then(function(result){
 
@@ -704,6 +714,7 @@
                         }
                         if(scope.intLoading==0) {
                             executeValidate();
+                            onFileEvent('readFile');
                         }
 					},function(error){
 
